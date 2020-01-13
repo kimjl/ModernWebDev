5c7da35ae006cc9afbd637ae5cc5bb1306acbd79
kimjl <jamesleekim90@gmail.com>
2020-01-13 18:21:07 -0500

    create utils file for debounce func

--- MovieFightDemo/index.js
+++ MovieFightDemo/index.js
@@ -1,1 +1,17 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: 'bc7f9707',
+			s: searchTerm
+		}
+	});
+
+	console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+const onInput = (event) => {
+	fetchData(event.target.value);
+};
+input.addEventListener('input', debounce(onInput, 500));

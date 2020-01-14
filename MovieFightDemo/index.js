8f71ddf72568c18035008ccbca8790cb8843ad06
kimjl <jamesleekim90@gmail.com>
2020-01-14 11:21:13 -0500

    create dropdown menu

--- MovieFightDemo/index.js
+++ MovieFightDemo/index.js
@@ -6,12 +6,43 @@ const fetchData = async (searchTerm) => {
 		}
 	});
 
-	console.log(response.data);
+	if (response.data.Error) {
+		return [];
+	}
+
+	return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
-const onInput = (event) => {
-	fetchData(event.target.value);
+const onInput = async (event) => {
+	const movies = await fetchData(event.target.value);
+
+	resultsWrapper.innerHTML = '';
+	dropdown.classList.add('is-active');
+	for (let movie of movies) {
+		const option = document.createElement('a');
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+		option.classList.add('dropdown-item');
+		option.innerHTML = `
+            <img src="${imgSrc}">
+            ${movie.Title}
+        `;
+
+		resultsWrapper.appendChild(option);
+	}
 };
 input.addEventListener('input', debounce(onInput, 500));

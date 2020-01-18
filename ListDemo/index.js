36174af6f1af2850d2a7581aa4c1d9632cc8abaa
kimjl <jamesleekim90@gmail.com>
2020-01-18 14:00:30 -0500

    add promise based solution

--- ListDemo/index.js
+++ ListDemo/index.js
@@ -1,7 +1,72 @@
 const fs = require('fs');
-fs.readdir('.', (err, filenames) => {
+const util = require('util');
+
+//METHOD 1 - WRAP LSTAT IN PROMISE
+// const lstat = (filename) => {
+// 	return new Promise((resolve, object) => {
+// 		fs.lstat(filename, (err, stats) => {
+// 			if (err) {
+// 				reject(err);
+// 			}
+// 			resolve(stats);
+// 		});
+// 	});
+// };
+
+//METHOD 2 - USE PROMISFY TO DO THE SAME AS WRAP LSTAT IN PROMISE
+// const lstat = util.promisify(fs.lstat);
+
+//METHOD 3 - RETURNS PROMISE INSTEAD OF CALLBACK
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
 	if (err) {
 		console.log(err);
 	}
-	console.log(filenames);
+
+	//PROMISE BASED SOLUTION ALL AT SAME TIME
+	const statPromises = filenames.map((filename) => {
+		return lstat(filename);
+	});
+
+	const allStats = await Promise.all(statPromises);
+
+	for (let stats of allStats) {
+		const index = allStats.indexOf(stats);
+		console.log(filenames[index], stats.isFile());
+	}
+
+	//PROMISE BASED SOLUTION ONE AT A TIME
+	// for (let filename of filenames) {
+	// 	try {
+	// 		const stats = await lstat(filename);
+
+	// 		console.log(filename, stats.isFile());
+	// 	} catch (err) {
+	// 		console.log(err);
+	// 	}
+	// }
+
+	//CALLBACK BASED SOLUTION
+	// const allStats = Array(filenames.length).fill(null);
+	// for (let filename of filenames) {
+	// 	const index = filenames.indexOf(filename);
+	// 	fs.lstat(filename, (err, stats) => {
+	// 		if (err) {
+	// 			console.log(err);
+	// 		}
+
+	// 		allStats[index] = stats;
+
+	// 		const ready = allStats.every((stats) => {
+	// 			return stats;
+	// 		});
+
+	// 		if (ready) {
+	// 			allStats.forEach((stats, index) => {
+	// 				console.log(filenames[index], stats.isFile());
+	// 			});
+	// 		}
+	// 	});
+	// }
 });

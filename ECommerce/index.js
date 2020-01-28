1c84036c5dfd5b8fa806f78ec74a1c722aba68e5
kimjl <jamesleekim90@gmail.com>
2020-01-28 14:51:31 -0500

    refactor and restructure

--- ECommerce/index.js
+++ ECommerce/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-const usersRepo = require('./repositories/users');
+const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
+app.use(express.static('public'));
 //parse form data for our app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -13,75 +15,8 @@ app.use(
 	})
 );
 
-app.get('/signup', (req, res) => {
-	res.send(`
-        <div>
-            Your ID is: ${req.session.userId}
-            <form method="POST">
-                <input name="email" placeholder="email">
-                <input name="password" placeholder="password">
-                <input name="passwordConfirmation" placeholder="password confirmation">
-                <button>Sign Up</button>
-            </form>
-        </div>
-    `);
-});
-
-app.post('/signup', async (req, res) => {
-	const { email, password, passwordConfirmation } = req.body;
-	const existingUser = await usersRepo.getOneBy({ email });
-	if (existingUser) {
-		res.send('Email in use');
-	}
-
-	if (password !== passwordConfirmation) {
-		return res.send('Passwords must match');
-	}
-
-	//Create a user in our repo to represent this person
-	const user = await usersRepo.create({ email, password });
-
-	//Store the id of that user inside the users cookie
-	req.session.userId = user.id; //req.session is object added in by cookie-session
-
-	res.send('Account created');
-});
-
-app.get('/signout', (req, res) => {
-	req.session = null;
-	res.send('You are logged out');
-});
-
-app.get('/signin', (req, res) => {
-	res.send(`
-        <div>
-            <form method="POST">
-                <input name="email" placeholder="email">
-                <input name="password" placeholder="password">
-                <button>Sign In</button>
-            </form>
-        </div>
-    `);
-});
-
-app.post('/signin', async (req, res) => {
-	const { email, password } = req.body;
-
-	const user = await usersRepo.getOneBy({ email });
-
-	if (!user) {
-		return res.send('Email not found');
-	}
-
-	const validPassword = await usersRepo.comparePassword(user.password, password);
-	if (!validPassword) {
-		return res.send('Invalid password');
-	}
-
-	req.session.userId = user.id;
-
-	res.send('You are signed in!');
-});
+app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
